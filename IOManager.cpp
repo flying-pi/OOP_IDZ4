e48c2a24f8378fc93e26d9263b4f267297677bd3
Yura Braiko <yurabraiko@gmail.com>
2017-01-27 22:34:36 +0200

    update

---
+++ IOManager.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Yura Braiko on 27.01.17.
+//
+
+#include "IOManager.h"
+
+short int IOManager::readFromkeyboard() {
+    return 0;
+}
+
+short int IOManager::nextRandomNum() {
+    return 0;
+}
---
+++ IOManager.h
@@ -0,0 +1,16 @@
+//
+// Created by Yura Braiko on 27.01.17.
+//
+
+#ifndef OOP_IDZ4_IOMANAGER_H
+#define OOP_IDZ4_IOMANAGER_H
+
+
+class IOManager {
+public:
+    short int readFromkeyboard();
+    short int nextRandomNum();
+};
+
+
+#endif //OOP_IDZ4_IOMANAGER_H
