d381f7f777693851af451a6eb0bcaefb4ab5bc0c
andrey484 <Veland29549>
2017-01-27 23:36:33 +0200

    create method to input array from keyboard and generation of elements

--- main.cpp
+++ main.cpp
@@ -10,8 +10,6 @@ int main() {
     short *array = new short[size];
     MyArray object(array, size);
 
-    //object.generationElements();
-    //object.inputFromKeyboard();
 
     for(int i = 0 ; i< size;i++){
         cout << array[i] << " ";
