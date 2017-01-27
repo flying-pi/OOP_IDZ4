f6ee8467297c73fcc3d3d92e8c2ecd8f1ff1d404
Yura Braiko <yurabraiko@gmail.com>
2017-01-27 22:30:17 +0200

    init

---
+++ MyArray.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Yura Braiko on 27.01.17.
+//
+
+#include "MyArray.h"
+
+MyArray::MyArray(short int *array, int size) {
+this->array = array;
+    this->size = size;
+}
---
+++ MyArray.h
@@ -0,0 +1,20 @@
+//
+// Created by Yura Braiko on 27.01.17.
+//
+
+#ifndef OOP_IDZ4_MYARRAY_H
+#define OOP_IDZ4_MYARRAY_H
+
+
+class MyArray {
+
+public:
+    MyArray(short int * array, int size);
+
+private:
+    short *array;
+    int size;
+};
+
+
+#endif //OOP_IDZ4_MYARRAY_H
